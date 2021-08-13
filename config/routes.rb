6f57da384440fed9eec0745d3fcb74ee93c5377c
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-13 17:30:55 +0800

    Add dashboard controller and basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
+
   root 'home#index'
   get '/home',          to: 'home#index'
+  get '/dashboard',     to: 'dashboard#index'
 end
