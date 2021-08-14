0437ea5b08113c453a3f8c62ecfc228e380f23a9
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-14 23:07:46 +0800

    Generate devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :user
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+}
 
   root 'home#index'
   get '/home',          to: 'home#index'
