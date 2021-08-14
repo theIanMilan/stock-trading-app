032cbcbb2212fd951246ca71f7499dc7512f5210
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-14 23:05:24 +0800

    After sign in; redirect to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    # override devise's same method
+    dashboard_path
+  end
 end
