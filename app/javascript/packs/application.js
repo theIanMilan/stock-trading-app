1ba7a7c9a49ae63dfa70e5bc1847a355ee42f2e6
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-17 19:44:02 +0800

    Add toastr style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Toastr
+global.toastr = require('toastr')
+import '../stylesheets/application'
