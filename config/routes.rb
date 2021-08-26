3bc8ec221f7d845212617d370396880f264faece
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-26 20:04:09 +0800

    Reconfigure devise routes; make login page the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations'
-}
 
-  root 'home#index'
+  devise_for :users, 
+             path: '',
+             path_names: { sign_in: '', sign_out: 'logout' }, 
+             controllers: {
+               sessions: 'users/sessions',
+               passwords: 'users/passwords',
+               registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   get '/home',          to: 'home#index'
   get '/dashboard',     to: 'dashboard#index'
 end
