6cc8f172257669630b07a45bf45996059adeb1e6
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-06 00:13:56 +0800

    Add validation that Order quantity cannot exceed total available stock quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   scope :sell_transactions, -> { where transaction_type: 'sell' }
 
   validate :total_buy_price_cannot_exceed_balance
+  validate :order_quantity_cannot_exceed_max_stock_quantity
 
   enum transaction_type: { buy: 0, sell: 1 }
 
@@ -16,5 +17,13 @@ class Order < ApplicationRecord
     return unless transaction_type == 'buy' && (user.balance < price * quantity)
 
     errors.add(:price, 'Insufficient funds with the given price and quantity.')
+  end
+
+  def order_quantity_cannot_exceed_max_stock_quantity
+    user = User.find(user_id)
+
+    return unless quantity > user.quantity
+
+    errors.add(:quantity, 'Order quantity cannot exceed total available stock quantity.')
   end
 end
