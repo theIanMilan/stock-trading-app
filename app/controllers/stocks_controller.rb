46affa595534249496429c0198220e3ab6b375bb
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-05 19:39:21 +0800

    Remove edit, update, and destroy methods in Stocks controller; reformat using flash and redirects

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,59 +1,30 @@
 class StocksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   load_and_authorize_resource # CanCan authorization helper
-  before_action :set_stock, only: %i[show edit update destroy]
+  before_action :set_stock, only: %i[show]
 
-  # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
   end
 
-  # GET /stocks/1 or /stocks/1.json
   def show; end
 
-  # GET /stocks/new
   def new
     @stock = Stock.new
   end
 
-  # GET /stocks/1/edit
-  def edit; end
-
-  # POST /stocks or /stocks.json
   def create
     @stock = Stock.new(stock_params)
-    respond_to do |format|
-      if @stock.save
-        # Associate with Broker
-        current_user.stocks << @stock
-        format.html { redirect_to @stock, notice: 'Stock was successfully created.' }
-        format.json { render :show, status: :created, location: @stock }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @stock.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /stocks/1 or /stocks/1.json
-  def update
-    respond_to do |format|
-      if @stock.update(stock_params)
-        format.html { redirect_to @stock, notice: 'Stock was successfully updated.' }
-        format.json { render :show, status: :ok, location: @stock }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @stock.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /stocks/1 or /stocks/1.json
-  def destroy
-    @stock.destroy
-    respond_to do |format|
-      format.html { redirect_to stocks_url, notice: 'Stock was successfully destroyed.' }
-      format.json { head :no_content }
+    if @stock.save
+      # Associate with Broker
+      current_user.stocks << @stock
+      # Update user_stocks
+      current_user.user_stocks.where(stock_id: @stock)
+                  .update(average_price: @stock.last_transaction_price, total_shares: @stock.quantity)
+      flash.notice = 'Stock was successfully added.'
+      redirect_to stock_path(@stock)
+    else
+      flash.alert = 'Failed: Error in adding Stock.'
     end
   end
 
