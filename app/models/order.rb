f9961ade22997144ba3e1e45f95d6d7b9115134b
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-05 23:31:01 +0800

    Add buy/sell scope to Order; Add validation that Order price * quantity will not exceed User cash balance

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,20 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+  scope :buy_transactions, -> { where transaction_type: 'buy' }
+  scope :sell_transactions, -> { where transaction_type: 'sell' }
+
+  validate :total_buy_price_cannot_exceed_balance
 
   enum transaction_type: { buy: 0, sell: 1 }
+
+  private
+
+  def total_buy_price_cannot_exceed_balance
+    user = User.find(user_id)
+
+    return unless transaction_type == 'buy' && (user.balance < price * quantity)
+
+    errors.add(:price, 'Insufficient funds with the given price and quantity.')
+  end
 end
