2ee0590aef9a6b08653a0e77dd0b98f764de4b20
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-06 21:27:11 +0800

    Add belongs to stock TransactionRecord

--- app/models/transaction_record.rb
+++ app/models/transaction_record.rb
@@ -1,4 +1,5 @@
 class TransactionRecord < ApplicationRecord
+  belongs_to :stock
   belongs_to :broker, class_name: 'User', dependent: :destroy, inverse_of: :transaction_records
   belongs_to :buyer, class_name: 'User', dependent: :destroy, inverse_of: :transaction_records
 
