f8d6e4107d5eeb82dce499f2b00f0b5af02d2feb
theIanMilan <theIanMilan@users.noreply.github.com>
2021-09-07 22:08:44 +0800

    Unreferenced let! calls removed; Redo rspec design

--- spec/system/order_matchings_spec.rb
+++ spec/system/order_matchings_spec.rb
@@ -5,16 +5,24 @@ RSpec.describe 'OrderMatchings', type: :system do
     driven_by(:rack_test)
   end
 
-  # Initialize Buyer
-  let!(:user)  { create(:user, :buyer) }
+  # # Initialize Buyer
+  # let!(:user)  { create(:user, :buyer) }
 
-  # Initialize Seller
-  let!(:user2) { create(:user, :broker) }
-  let!(:stock) { create(:stock, :p10_q1000) }
-  let!(:user_stock) { create(:user_stock, user: user2, stock: stock) }
-  let!(:seller_order) { create(:order, user: user2, stock: stock) }
+  # # Initialize Seller
+  # let!(:user2) { create(:user, :broker) }
+  # let!(:stock) { create(:stock, :p10_q1000) }
+  # let!(:user_stock) { create(:user_stock, user: user2, stock: stock) }
+  # let!(:seller_order) { create(:order, user: user2, stock: stock) }
 
   it 'buy order that matches sell order executes' do
+    # Seller
+    user2 = create(:user, :broker)
+    stock = create(:stock, :p10_q1000)
+    create(:user_stock, user: user2, stock: stock)
+    create(:order, user: user2, stock: stock)
+
+    # Buyer
+    user = create(:user, :buyer)
     create(:order, :buying, user: user, stock: stock)
     expect(Order.count).to eq(0)
   end
